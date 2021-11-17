091882aba42e97635a4fac06555dbefae7844fcb
gitasintya <gitasintyap@gmail.com>
2021-11-17 11:54:05 +0700

    first commit

---
+++ BirthdayPage/script.js
@@ -0,0 +1,17 @@
+alert("HALOOO AFNIII!");
+alert("Ada sesuatuu nihh :'))");
+alert("Gak spesial kok, gak pake telurr XD");
+alert("Eh bentar berdoa duluuu XD");
+alert("Udah??");
+alert("OKEOKEE Let's Begin");
+window.location.href="start.html";
+  	// if(a== "afni" && b=="17-11-1997")
+  	// {
+  	//     window.location.href="start.html";
+  	// }
+  	// else
+  	// {
+  	// 	alert("invalid details");
+  	// 	window.location.href="start.html";
+  	// }
+  	
